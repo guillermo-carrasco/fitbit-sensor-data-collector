d45369831d451b87412daa02be517308adbaa120
guillermo-carrasco <guillermo.carrasco@izettle.com>
2021-04-13 14:14:36 +0200

    Adds gyroscope and orientation data

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# fitbit-sensor-data-collector
-Collects sensor data from a Fitbit smartwatch and stores it in your phone
+# Fitbit Sensor Data Collector
+
+Collects sensor data from a Fitbit smartwatch and stores it in your phone. App developed for Fitbit Sense. Will
+work on other devices, but for example gyroscope data might not be available (as is the case for Versa devices).
+
+## Installation
+
+### Using CLI
+
+Read more on how to install CLI [here](https://dev.fitbit.com/build/guides/command-line-interface/)
+
+1. In the phone app, go to Account, select your device -> Developer Menu -> Toggle Developer Bridge
+2. In the watch: Settings -> Developer Bridge -> Toggle enable
+
+**Build the app** with `fitbit-build`
+
+**Install the app**: Enter fitbil CLI interface with `fitbit` command, then just type `install`
--- app/index.js
+++ app/index.js
@@ -1,5 +1,7 @@
 import { Accelerometer } from "accelerometer";
 import { Barometer } from "barometer";
+import { Gyroscope } from "gyroscope";
+import { OrientationSensor } from "orientation";
 
 import * as messaging from "messaging";
 
@@ -18,30 +20,74 @@ messaging.peerSocket.addEventListener("error", (err) => {
 // Start data collection in intervals of 100 milliseconds
 function sensorDataCollection() {
 
-  let accel_data = {x: 0, y:0, z:0}
+  // Initialise sensor data holders
+  let accel_data = {x: null, y: null, z: null}
+  let baro_data = {pressure: null}
+  let gyro_data = {x: null, y: null, z: null}
+  let orient_data = {quaternion: null}
 
-  // Collects accelerometer data
+  // Start sensors at the same sampling frequency than the interval we send messages to the companion app
   if (Accelerometer) {
     const accel = new Accelerometer({ frequency: FREQUENCY_INTERVALS_IN_MS });
     accel.addEventListener("reading", () => {
       accel_data = {
-        x: accel.x,
-        y: accel.y,
-        z: accel.z
+          x: accel.x ? accel.x.toFixed(3) : 0,
+          y: accel.y ? accel.y.toFixed(3) : 0,
+          z: accel.z ? accel.z.toFixed(3) : 0
       };
     });
     accel.start();
   }
+  if (Barometer) {
+      const barometer = new Barometer({ frequency: FREQUENCY_INTERVALS_IN_MS });
+      barometer.addEventListener("reading", () => {
+        baro_data = {
+          pressure: barometer.pressure ? parseInt(barometer.pressure) : 0
+        };
+      });
+      barometer.start();
+  }
+  if (Gyroscope) {
+    const gyro = new Gyroscope({ frequency: FREQUENCY_INTERVALS_IN_MS });
+    gyro.addEventListener("reading", () => {
+      gyro_data = {
+        x: gyro.x ? gyro.x.toFixed(3) : 0,
+        y: gyro.y ? gyro.y.toFixed(3) : 0,
+        z: gyro.z ? gyro.z.toFixed(3) : 0,
+      };
+      console.log(
+        `ts: ${gyro.timestamp}, \
+         x: ${gyro.x}, \
+         y: ${gyro.y}, \
+         z: ${gyro.z}`
+      );
+    });
+    gyro.start();
+  }
+  if (OrientationSensor) {
+    const orientation = new OrientationSensor({ frequency: FREQUENCY_INTERVALS_IN_MS });
+    orientation.addEventListener("reading", () => {
+      orient_data = {
+        quaternion: orientation.quaternion ? orientation.quaternion.map(n => n.toFixed(3)) : null
+      };
+    });
+    orientation.start();
+  }
 
+
+  // Send a new data sample to the companion app every interval. The variables are updated asyncronously
   setInterval(() => {
       if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
-        // Send the data to peer as a message
-      messaging.peerSocket.send(
-        {
+        messaging.peerSocket.send({
           timestamp: new Date().getTime(),
           accel_x: accel_data.x,
           accel_y: accel_data.y,
           accel_z: accel_data.z,
+          baro_pressure: baro_data.pressure,
+          gyro_x: gyro_data.x,
+          gyro_y: gyro_data.y,
+          gyro_z: gyro_data.z,
+          orientation: orient_data.quaternion
         });
       }
     }, FREQUENCY_INTERVALS_IN_MS);
